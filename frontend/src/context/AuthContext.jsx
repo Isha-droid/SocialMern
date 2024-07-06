8e6a200b101acbd229d5a809880f283856d3681a
Isha Mahajan <mahajanisha508@gmail.com>
2024-07-06 18:23:52 +0530

    reloading updated

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,10 +1,8 @@
-// AuthProvider.js
-
 import React, { useReducer, useEffect, createContext } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-  user: localStorage.getItem("user") ,
+  user: JSON.parse(localStorage.getItem("user")) || null,
   isFetching: false,
   error: false,
 };
@@ -14,9 +12,23 @@ export const AuthContext = createContext(INITIAL_STATE);
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
+  // Fetch user data from localStorage when component mounts
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      try {
+        const userData = JSON.parse(storedUser);
+        dispatch({ type: "LOGIN_SUCCESS", payload: userData });
+      } catch (error) {
+        console.error("Error parsing user data from localStorage:", error);
+        // Optionally handle error or fallback gracefully
+      }
+    }
+  }, []);
+
   // Save user to localStorage whenever state.user changes
   useEffect(() => {
-    console.log(state.user)
+    localStorage.setItem("user", JSON.stringify(state.user));
   }, [state.user]);
 
   return (
